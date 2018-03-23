10d3ba92427b5c7bf26b29a23279e55b6abe9083
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-23 15:56:47 -0700

    Add GET /users and POST /user paths to hit DB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
 const Promise = require('bluebird');
 const cors = require('cors');
 const groupGenerator = require('../database');
 const path = require('path');
+const db = require('../database/index');
 const PORT = '8080';
 
 const app = express();
@@ -13,24 +13,26 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyParser.json());
 app.use(cors());
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: ''
-});
-
-db.connect((err) => {
-  if (err) console.log('Error connecting...', err)
-  console.log('Woohoo! Connected to the DB!')
-  db.query('CREATE DATABASE IF NOT EXISTS groupGenerator', (err) => {
-    if (err) console.log('Error creating DB: ', err);
-    db.query('USE groupGenerator', (err) => {
-      if (err) console.log('Error using DB: ', err);
-    });
-  });
-});
-
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`)
   console.log(`Visit 127.0.0.1:${PORT} to view app`)
+});
+
+app.get('/users', (req, res) => {
+  console.log('Getting users');
+  db.retrieve()
+    .then((response) => {
+      res.status(200).send(response);
+    });
+});
+
+app.post('/user', (req, res) => {
+  const user = req.body.user;
+  console.log('Posting user: ', user);
+  db.add(user)
+    .then(() => res.status(200).send('Saved user!'))
+    .catch((err) => {
+      res.status(500).send('Error saving user');
+      console.log(err);
+    });
 });
