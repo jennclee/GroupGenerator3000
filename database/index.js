50a87a1ffca62f58a02d6ff2805f70f463f439d7
jennclee <8753352+jennclee@users.noreply.github.com>
2018-03-24 23:33:02 -0700

    Add groups table, user id retrieval, and history retrieval DB functions

--- database/index.js
+++ database/index.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql');
-const Promise = require('bluebird');
 
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: ''
 });
+
+const userSQL = 'CREATE TABLE IF NOT EXISTS users(id INTEGER AUTO_INCREMENT NOT NULL, name VARCHAR(30) NOT NULL UNIQUE,PRIMARY KEY(id))';
+
+const groupSQL = 'CREATE TABLE IF NOT EXISTS groups(id INTEGER, user_id INTEGER, history_id INTEGER, PRIMARY KEY(id), FOREIGN KEY (user_id) REFERENCES users(id))';
 
 db.connect((err) => {
   if (err) console.log('Error connecting...', err);
@@ -15,50 +18,97 @@ db.connect((err) => {
     db.query('USE groupGenerator', (err) => {
       if (err) console.log('Error using DB: ', err);
       db.query('DROP TABLE IF EXISTS users', (err) => {
-        if (err) console.log('Error dropping user table');
-        db.query('CREATE TABLE IF NOT EXISTS users(id INTEGER AUTO_INCREMENT NOT NULL, name VARCHAR(30) NOT NULL,PRIMARY KEY(id))', (err) => {
-          if (err) console.log('Error creating user table');
+        if (err) console.log('Error dropping users table');
+        db.query(userSQL, (err) => {
+          if (err) console.log('Error creating users table');
+          db.query('DROP TABLE IF EXISTS groups', (err) => {
+            if (err) console.log('Error dropping groups table');
+            db.query(groupSQL, (err) => {
+              if (err) console.log('Error creating groups table: ', err);
+            });
+          });
         });
       });
     });
   });
 });
 
-module.exports.add = (name) => {
-  const sql = 'INSERT IGNORE INTO users (name) VALUES (?)';
-  return new Promise((resolve, reject) => {
-    db.query(sql, `${name}`, (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results);
-      }
+module.exports = {
+  add: (name) => {
+    const sql = 'REPLACE INTO users (name) VALUES (?)';
+    return new Promise((resolve, reject) => {
+      db.query(sql, `${name}`, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
     });
-  });
-};
+  },
 
-module.exports.retrieve = () => {
-  const sql = 'SELECT * FROM users';
-  return new Promise((resolve, reject) => {
-    db.query(sql, (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results);
-      }
+  retrieveUsers: () => {
+    const sql = 'SELECT * FROM users';
+    return new Promise((resolve, reject) => {
+      db.query(sql, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
     });
-  });
-};
+  },
 
-module.exports.remove = (name) => {
-  const sql = 'DELETE FROM users WHERE name = ?';
-  return new Promise((resolve, reject) => {
-    db.query(sql, `${name}`, (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results);
-      }
+  retrieveUser: (id) => {
+    const sql = 'SELECT * FROM users WHERE id = ?';
+    return new Promise((resolve, reject) => {
+      db.query(sql, `${id}`, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
     });
-  });
+  },
+
+  remove: (name) => {
+    const sql = 'DELETE FROM users WHERE name = ?';
+    return new Promise((resolve, reject) => {
+      db.query(sql, `${name}`, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  },
+
+  retrieveHistory: () => {
+    const sql = 'SELECT * FROM groups GROUP BY history_id';
+    return new Promise((resolve, reject) => {
+      db.query(sql, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  },
+
+  addGroup: (users) => {
+    const sql = 'INSERT IGNORE INTO groups (id, user_id, history_id) VALUES ?';
+    return new Promise((resolve, reject) => {
+      db.query(sql, [users], (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  },
 };
