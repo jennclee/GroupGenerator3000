f3d598a9ea81f2c20b25844096bbfacbe57ac793
Jennifer Lee <8753352+jennclee@users.noreply.github.com>
2018-03-27 22:30:34 -0700

    Remove old comments

--- client/src/components/CreateGroups.jsx
+++ client/src/components/CreateGroups.jsx
@@ -1,55 +1,5 @@
 import React from 'react';
 
-
-// const createGroups = (min, group) => {
-//   const randomizedGroup = {};
-//   for (let i = 0; i < group.length; i++) {
-//     const idx = Math.random();
-//     randomizedGroup[group[i]] = idx;
-//   }
-//   const sortableGroup = [];
-//   for (let key in randomizedGroup) {
-//     sortableGroup.push([key, randomizedGroup[key]]);
-//   }
-//   const sortedGroup = sortableGroup.sort((a, b) => {
-//     return a[1] - b[1];
-//   });
-
-//   const groupings = [];
-//   let miniGroup = [];
-//   let position = 0;
-//   const leftover = group.length % min;
-//   for (let j = 0; j < sortedGroup.length - leftover; j++) {
-//     if (miniGroup.length === min - 1) {
-//       miniGroup.push(sortedGroup[j][0]);
-//       groupings.push(miniGroup);
-//       miniGroup = [];
-//       position++;
-//     } else if (miniGroup.length < min - 1) {
-//       miniGroup.push(sortedGroup[j][0]);
-//       position++;
-//     }
-//   }
-
-//   if (leftover === min - 1 && leftover !== 1) {
-//     const leftoverGroup = [];
-//     for (position; position < group.length; position++) {
-//       leftoverGroup.push(sortedGroup[position][0]);
-//     }
-//     groupings.push(leftoverGroup);
-//   } else if (leftover !== 0) {
-//     if (leftover === 1 && min === 2) {
-//       position--;
-//     }
-//     for (let k = 0; k < leftover; k++) {
-//       groupings[k].push(sortedGroup[position][0]);
-//       position++;
-//     }
-//   }
-
-//   return groupings;
-// };
-
 const createGroups = (min, group) => {
   let temp;
   const groupings = [];
